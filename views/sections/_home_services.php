62fca1f62a64e43be88b0ec60f48806786cf4af1
AresRisingUA <46715078+AresRisingUA@users.noreply.github.com>
2019-04-25 14:13:55 +0300

    htaccess added

--- views/sections/_home_services.php
+++ views/sections/_home_services.php
@@ -3,30 +3,34 @@
         <h2 class="home_services__header"><span class="home_services__header-bold">Services</span> include</h2>
         <div class="home_serices_list-wrapper">
             <div class="home_services_col-1">
-                <img class="home_services__list-icon" src="<?php echo $imgPath.'puzzle.svg'?>" alt="Search team 7 services icon">
-                <h3 class="home_services_list__header">Creative</h3>
-                <ul class="home_services_list">
-                    <li class="home_services_list__item">Branding/logo design</li>
-                    <li class="home_services_list__item">Conversion optimization and copywriting</li>
-                    <li class="home_services_list__item">Marketing strategy</li>
-                </ul>
+                <div class="home_services_col-1_upper">
+                    <img class="home_services__list-icon" src="<?php echo $imgPath.'puzzle.svg'?>" alt="Search team 7 services icon">
+                    <h3 class="home_services_list__header">Creative</h3>
+                </div>
+                <div class="home_services_col-1_lower">
+                    <ul class="home_services_list">
+                        <li class="home_services_list__item"><span>Branding/logo design</span></li>
+                        <li class="home_services_list__item"><span>Conversion optimization and copywriting</span></li>
+                        <li class="home_services_list__item"><span>Marketing strategy</span></li>
+                    </ul>
+                </div>
             </div>
             <div class="home_services_col-2">
                 <img class="home_services__list-icon" src="<?php echo $imgPath.'megaphone.svg'?>" alt="Search team 7 services icon">
                 <h3 class="home_services_list__header">Marketing & Advertising</h3>
                 <ul class="home_services_list">
-                    <li class="home_services_list__item">Search Engine Marketing (PPC)</li>
-                    <li class="home_services_list__item">Search Engine Optimization (SEO)</li>
-                    <li class="home_services_list__item">Social Media marketing (SMM)</li>
+                    <li class="home_services_list__item"><span>Search Engine Marketing (PPC)</span></li>
+                    <li class="home_services_list__item"><span>Search Engine Optimization (SEO)</span></li>
+                    <li class="home_services_list__item"><span>Social Media marketing (SMM)</span></li>
                 </ul>
             </div>
             <div class="home_services_col-3">
                 <img class="home_services__list-icon" src="<?php echo $imgPath.'web.svg'?>" alt="Search team 7 services icon">
                 <h3 class="home_services_list__header">Development</h3>
                 <ul class="home_services_list">
-                    <li class="home_services_list__item">Web design and development</li>
-                    <li class="home_services_list__item">Mobile design and development</li>
-                    <li class="home_services_list__item">CRM set up and integrations and funnel set</li>
+                    <li class="home_services_list__item"><span>Web design and development</span></li>
+                    <li class="home_services_list__item"><span>Mobile design and development</span></li>
+                    <li class="home_services_list__item"><span>CRM set up and integrations and funnel set</span></li>
                 </ul>
             </div>
         </div>
