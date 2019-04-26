920941f21177d673fe2d7428a4af373972db275f
Vincmark <44532137+Vincmark@users.noreply.github.com>
2019-04-26 23:55:53 +0300

    0000

--- views/sections/_our-process_process.php
+++ views/sections/_our-process_process.php
@@ -1,1 +1,32 @@
-<?php
+<section class="our-process_process">
+    <div class="our-process_process_wrapper">
+        <h2 class="typo-h2 typo_white-color">Here is how our <span class="typo_h-bold typo_green-color">clients onboarding process works</span></h2>
+        <p>Schedule your complimentary Marketing strategy session with one of our specialists.</p>
+        <p>During the session we will fill out a brief questionnaire to get an understanding of what you need in order to grow your business. Here are some things we need to understand:</p>
+        <ul class="our-process_why_list">
+            <li>Your business model</li>
+            <li>Your Competitors</li>
+            <li>Your Revenue model</li>
+            <li>Your Conversion strategy</li>
+            <li>Your Budget</li>
+            <li>Most Importantly Your Goals</li>
+        </ul>
+        <div class="our-process_line-separator"></div>
+        <div>
+            <img src="" alt="">
+            <p>We will present you with a marketing plan and the services that we can provide to your company in order to make it grow.</p>
+        </div>
+        <div class="our-process_line-separator"></div>
+        <div>
+            <img src="" alt="">
+            <p>You will choose which of the suggested services you want for your business.</p>
+        </div>
+        <div class="our-process_line-separator"></div>
+        <div>
+            <img src="" alt="">
+            <p>We get to work!</p>
+        </div>
+        <a class="not-found-404_button button-gradient">Here is a Breakdown</a>
+        <p>of the services we provide</p>
+    </div>
+</section>
--- views/sections/_our-process_why-build-website.php
+++ views/sections/_our-process_why-build-website.php
@@ -7,37 +7,25 @@
     </div>
     <div class="our-process_why-build-website_wrapper">
         <h2 class="typo-h2 typo_dark-color">Why do you want to <span class="typo_h-bold">build a website</span> and <span class="typo_h-bold">market online?</span></h2>
-        <ul>
-            <li>Is your website simply a “business card” to help you convert or do you plan to generate leads and sales from your site?</li>
-            <li>Who are your clients? Who are your competitors? What are they doing to get clients?</li>
-            <li>Before we begin working together we need to get clear on key information like this.</li>
+        <ul class="our-process_why_list">
+            <li class="our-process_why_item typo-p typo_dark-color">Is your website simply a “business card” to help you convert or do you plan to generate leads and sales from your site?</li>
+            <li class="our-process_why_item typo-p typo_dark-color">Who are your clients? Who are your competitors? What are they doing to get clients?</li>
+            <li class="our-process_why_item typo-p typo_dark-color">Before we begin working together we need to get clear on key information like this.</li>
         </ul>
         <h3 class="typo-h3 typo_dark-color">In order to achieve this <span class="typo_h-bold">we created a 5 steps</span> process that will <span class="typo_h-bold">tell us 3 critical things:</span></h3>
-        <ul>
-            <li>
-                <img src="../img/our-process-1.svg" alt="Thing 1">
-                <p>Are we the right fit for each other?</p>
+        <ul class="our-process_things_list">
+            <li class="our-process_things_item">
+                <img class="our-process_things_img" src="<?= $imgPath.'our-process-1.svg'?>" alt="Thing 1">
+                <p class="our-process_things_p">Are we the right fit for each other?</p>
             </li>
-            <li>
-                <img src="../img/our-process-2.svg" alt="Thing 2">
-                <p>What do you need to do to get more customers and grow your business.</p>
+            <li class="our-process_things_item">
+                <img class="our-process_things_img" src="<?= $imgPath.'our-process-2.svg'?>" alt="Thing 2">
+                <p class="our-process_things_p">What do you need to do to get more customers and grow your business.</p>
             </li>
-            <li>
-                <img src="../img/our-process-3.svg" alt="Thing 3">
-                <p>How can we help you get more customers and grow your business.</p>
+            <li class="our-process_things_item">
+                <img src="<?= $imgPath.'our-process-3.svg'?>" alt="Thing 3">
+                <p class="our-process_things_p">How can we help you get more customers and grow your business.</p>
             </li>
         </ul>
-
-        <div>
-            <img src="<?= $imgPath.'dm-landscape-image.svg'?>" alt="Digital Marketing Landscape image">
-            <div>
-                <p class="typo-p typo_dark-color"><span class="typo_p-bold">Everybody knows that online marketing is a great way to promote your business.</span></p>
-                <p class="typo-p typo_dark-color">But, with total advertising spend of <span class="typo_p-bold">7.85 Billion dollars</span> on social media <span class="typo_p-bold">$34.42 billion</span> on Google in 2018 alone  it is clear that there is a lot of competition.</p>
-                <p class="typo-p typo_dark-color">When marketing online the difference between growing your business and wasting your time is in the details.</p>
-                <p class="typo-p typo_dark-color">In order to get these details right you need true specialists...</p>
-                <p class="typo-p typo_dark-color">Experienced specialists that know digital marketing inside and out and made millions marketing online.</p>
-                <p class="typo-p typo_dark-color">This is where we come in….</p>
-            </div>
-        </div>
     </div>
 </section>
