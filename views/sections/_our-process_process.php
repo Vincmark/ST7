82b4deb6bc0c4c543591db15dacf73e966ae6755
Alexey Koshevoy <gphtechak@gmail.com>
2019-04-27 02:56:53 +0300

    3000

--- views/sections/_our-process_process.php
+++ views/sections/_our-process_process.php
@@ -1,32 +1,35 @@
 <section class="our-process_process">
     <div class="our-process_process_wrapper">
         <h2 class="typo-h2 typo_white-color">Here is how our <span class="typo_h-bold typo_green-color">clients onboarding process works</span></h2>
-        <p>Schedule your complimentary Marketing strategy session with one of our specialists.</p>
-        <p>During the session we will fill out a brief questionnaire to get an understanding of what you need in order to grow your business. Here are some things we need to understand:</p>
-        <ul class="our-process_why_list">
-            <li>Your business model</li>
-            <li>Your Competitors</li>
-            <li>Your Revenue model</li>
-            <li>Your Conversion strategy</li>
-            <li>Your Budget</li>
-            <li>Most Importantly Your Goals</li>
-        </ul>
-        <div class="our-process_line-separator"></div>
         <div>
-            <img src="" alt="">
-            <p>We will present you with a marketing plan and the services that we can provide to your company in order to make it grow.</p>
+            <img src="<?= $imgPath.'our-process-num1.svg'?>" alt="">
+            <p class="our-process_green-text">Schedule your complimentary Marketing strategy session with one of our specialists.</p>
+            <p class="our-process_white-text">During the session we will fill out a brief questionnaire to get an understanding of what you need in order to grow your business. Here are some things we need to understand:</p>
+            <ul class="our-process_questions_list">
+                <li class="our-process_questions_item">Your business model</li>
+                <li class="our-process_questions_item">Your Competitors</li>
+                <li class="our-process_questions_item">Your Revenue model</li>
+                <li class="our-process_questions_item">Your Conversion strategy</li>
+                <li class="our-process_questions_item">Your Budget</li>
+                <li class="our-process_questions_item">Most Importantly Your Goals</li>
+            </ul>
         </div>
         <div class="our-process_line-separator"></div>
         <div>
-            <img src="" alt="">
-            <p>You will choose which of the suggested services you want for your business.</p>
+            <img src="<?= $imgPath.'our-process-num2.svg'?>" alt="">
+            <p class="our-process_green-text">We will present you with a marketing plan and the services that we can provide to your company in order to make it grow.</p>
         </div>
         <div class="our-process_line-separator"></div>
         <div>
-            <img src="" alt="">
-            <p>We get to work!</p>
+            <img src="<?= $imgPath.'our-process-num3.svg'?>" alt="">
+            <p class="our-process_green-text">You will choose which of the suggested services you want for your business.</p>
         </div>
-        <a class="not-found-404_button button-gradient">Here is a Breakdown</a>
-        <p>of the services we provide</p>
+        <div class="our-process_line-separator"></div>
+        <div>
+            <img src="<?= $imgPath.'our-process-num4.svg'?>" alt="">
+            <p class="our-process_big-green-text">We get to work!</p>
+        </div>
+        <a class="button-gradient">Here is a Breakdown</a>
+        <p class="our-process_button-subtext">of the services we provide</p>
     </div>
 </section>
