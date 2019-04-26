f9ef3cb02f9295fb04f3464e5e7d60a409ce30d0
AresRisingUA <46715078+AresRisingUA@users.noreply.github.com>
2019-04-26 15:20:57 +0300

    Separated header

--- views/sections/_home_request-form.php
+++ views/sections/_home_request-form.php
@@ -2,9 +2,9 @@
     <div class="home_request-form_wrapper">
         <p class="home_request-form__header typo_dark-color typo_bold">Free 30 minute Digital Marketing Consultation</p>
         <div class="home_request-form_input-group">
-            <input type="text" class="home_request-form__input-name typo_dark-color typo-p typo_bold" name="home_request-form__input-name" value="First Name" >
-            <input type="email" class="home_request-form__input-email typo_dark-color typo-p typo_bold" name="home_request-form__input-email"value="Email">
-            <input type="button" class="home_request-form__input-button typo_white-color typo-p typo_bold" value="Schedule my free consultation">
+            <input type="text" class="home_request-form__input-name typo_dark-color typo-p typo_p-bold" name="home_request-form__input-name" value="First Name" onfocus="if (this.value == 'First Name') {this.value=''}" onBlur="if (this.value == '') {this.value='First Name'}">
+            <input type="email" class="home_request-form__input-email typo_dark-color typo-p typo_p-bold" name="home_request-form__input-email"value="Email" onfocus="if (this.value == 'Email') {this.value=''}" onBlur="if (this.value == '') {this.value='Email'}">
+            <input type="button" class="home_request-form__input-button typo_white-color typo-p typo_p-bold" value="Schedule my free consultation">
         </div>
     </div>
 </form>
--- views/sections/_splash.php
+++ views/sections/_splash.php
@@ -1,37 +1,7 @@
 <section class="splash">
     <div class="splash-wrapper">
         <h1 class="visually-hidden">Search Team 7 official website</h1>
-        <header class="header">
-            <img class="header__logo" src="<?= $imgPath.'logo.svg'?>" alt="Search team 7 logo" height="119" width="131">
-            <div class="header-nav-wrapper">
-                <nav class="header-menu__wrapper">
-                    <ul class="header-menu">
-                        <li class="header-menu__menu-item"><a href="#">Home</a></li>
-                        <li class="header-menu__menu-item"><a href="#">About</a></li>
-                        <li class="header-menu__menu-item"><a href="#">Services</a></li>
-                        <li class="header-menu__menu-item"><a href="#">Portfolio</a></li>
-                        <li class="header-menu__menu-item"><a href="#">Our process</a></li>
-                        <li class="header-menu__menu-item"><a href="#">Blog</a></li>
-                    </ul>
-                </nav>
-                <a class="header__button button-transparent" href="#id_contact-us">Contact</a>
-
-            </div>
-            <button class="header-menu-toggler" id="header-menu-toggler"></button>
-            <div class="header-mobile-menu__wrapper" id="mobile-menu-wrapper">
-
-                <button class="header-mobile-menu__close-button" id="mobile-menu__close-button"></button>
-                <ul class="header-mobile-menu">
-                    <li class="header-mobile-menu__menu-item"><a href="#">Home</a></li>
-                    <li class="header-mobile-menu__menu-item"><a href="#">About</a></li>
-                    <li class="header-mobile-menu__menu-item"><a href="#">Services</a></li>
-                    <li class="header-mobile-menu__menu-item"><a href="#">Portfolio</a></li>
-                    <li class="header-mobile-menu__menu-item"><a href="#">Our process</a></li>
-                    <li class="header-mobile-menu__menu-item"><a href="#">Blog</a></li>
-                    <li class="header-mobile-menu__menu-item"><a href="#">Contact us</a></li>
-                </ul>
-            </div>
-        </header>
+        <?php include("_header.php");?>
         <div class="splash-content">
             <p class="splash-content__description">We are going to create <span class="splash-content__description-green">a results driven optimal marketing strategy</span> for your business</p>
         </div>
