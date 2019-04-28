3bdfac757bc273c7209910f59b2b444ac512fd61
Vincmark <44532137+Vincmark@users.noreply.github.com>
2019-04-28 22:54:09 +0300

    28apr

--- views/sections/_services_design.php
+++ views/sections/_services_design.php
@@ -1,1 +1,14 @@
-<?php
+<section class="services_design">
+    <div class="services_design_wrapper">
+        <h2 class="typo-h2"><span class="typo_h-bold">Design</span> and <span class="typo_h-bold">Branding</span></h2>
+        <img class="services_design_img1" src="<?= $imgPath.'services_design_img1.svg'?>" alt="Design image">
+        <p class="typo-p typo_p-bold">Everybody wants to have their logo, website and company brand to look professional.</p>
+        <p class="typo-p typo_p-bold">But there is a lot more to branding than just design.</p>
+        <p class="typo-p">Most businesses need what is known as a <span class="typo_p-bold"> brand book</span>. This means aesthetic consistency in your logo, website, product look and feel and even the fonts used on your site.</p>
+        <p class="typo-p typo_p-bold">This will help you generate more clients, and help you make your customers more stick.</p>
+
+        <p class="button_upper-text typo_orange-color">If you need branding or logo design we can help.</p>
+        <a class="button-gradient">Set up a Free Consultation</a>
+        <p class="button_lower-text typo_dark-color">to find out how</p>
+    </div>
+</section>
---
+++ views/sections/_services_sales-funnel.php
@@ -0,0 +1,16 @@
+<section class="services_sales-funnel">
+    <div class="services_sales-funnel_wrapper">
+        <h2 class="typo-h2 typo_white-color">Sales Funnel <span class="typo_h-bold typo_green-color">Creation</span> and <span class="typo_h-bold typo_green-color">Optimization</span></h2>
+        <img class="services_sales-funnel_img1" src="<?= $imgPath.'services_sales-funnel_img1.svg'?>" alt="Funnel image">
+        <p class="typo-p typo_white-color typo_p-bold">There is a lot more to converting a visitor to a customer than just your website.</p>
+        <p class="typo-p typo_white-color">The key is to properly stay in front of your prospects so that you are there to provide your service when he/she is ready to take action.</p>
+        <p class="typo-p typo_white-color">In order to do this you need to <span class="typo_p-bold">set up a CRM</span> and create a proper follow up campaign using <span class="typo_p-bold typo_green-color">MARKETING AUTOMATION</span>.</p>
+        <p class="typo-p typo_white-color">This can mean a number of things such as follow up emails, browser push notifications, text message follow ups, exit alerts, webinars and even good old fashioned smiling and dialing.</p>
+        <img class="services_sales-funnel_img2" src="<?= $imgPath.'services_sales-funnel_img2.svg'?>" alt="Conversion image">
+        <p class="typo-p typo_white-color">The good news is that this is where we shine. <span class="typo_p-bold">We have been successfully creating and implementing sales funnels since 2005.</span></p>
+        <p class="typo-p typo_white-color">We are experts in tools such as <span class="typo_p-bold">Infusionsoft, Get Response, Everwebinar, Salesforce, Active Campaign and more.</span></p>
+        <p class="typo-p typo_white-color">We will work together with you to map out your sales journey and than use the necessary tools above to maximize your conversion.</p>
+        <a class="button-gradient">Schedule a Free Marketing Consultation</a>
+        <p class="button_lower-text">in order to plan your sales funnel today.</p>
+    </div>
+</section>
