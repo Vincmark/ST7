68895b1fdc3a08b17d3028ee99094d54af14abe6
Vincmark <44532137+Vincmark@users.noreply.github.com>
2019-05-01 00:35:45 +0300

    0000

--- views/sections/_our-process_process.php
+++ views/sections/_our-process_process.php
@@ -1,35 +1,55 @@
 <section class="our-process_process">
     <div class="our-process_process_wrapper">
-        <h2 class="typo-h2 typo_white-color">Here is how our <span class="typo_h-bold typo_green-color">clients onboarding process works</span></h2>
-        <div>
-            <img src="<?= $imgPath.'our-process-num1.svg'?>" alt="">
-            <p class="our-process_green-text">Schedule your complimentary Marketing strategy session with one of our specialists.</p>
-            <p class="our-process_white-text">During the session we will fill out a brief questionnaire to get an understanding of what you need in order to grow your business. Here are some things we need to understand:</p>
-            <ul class="our-process_questions_list">
-                <li class="our-process_questions_item">Your business model</li>
-                <li class="our-process_questions_item">Your Competitors</li>
-                <li class="our-process_questions_item">Your Revenue model</li>
-                <li class="our-process_questions_item">Your Conversion strategy</li>
-                <li class="our-process_questions_item">Your Budget</li>
-                <li class="our-process_questions_item">Most Importantly Your Goals</li>
-            </ul>
+        <h2 class="our-process_why-build-website_h2 typo-h2 typo_white-color">Here is how our <span class="typo_h-bold typo_green-color">clients onboarding process works</span></h2>
+        <div class="center-content">
+            <div class="our-process_process_step no-top-padding">
+                <p class="our-process_process_step_num">1</p>
+                <div class="our-process_process_step_content">
+                    <p class="our-process_green-text">Schedule your complimentary Marketing strategy session with one of our specialists.</p>
+                    <p class="our-process_white-text">During the session we will fill out a brief questionnaire to get an understanding of what you need in order to grow your business. Here are some things we need to understand:</p>
+                    <div class="our-process_questions_list">
+                        <div class="our-process_questions_list_col">
+                            <p class="our-process_questions_item typo-ps">Your business model</p>
+                            <p class="our-process_questions_item typo-ps">Your Competitors</p>
+                            <p class="our-process_questions_item typo-ps">Your Revenue model</p>
+                        </div>
+                        <div class="our-process_questions_list_col">
+                            <p class="our-process_questions_item typo-ps">Your Conversion strategy</p>
+                            <p class="our-process_questions_item typo-ps">Your Budget</p>
+                            <p class="our-process_questions_item typo-ps">Most Importantly Your Goals</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="our-process_line-separator"></div>
-        <div>
-            <img src="<?= $imgPath.'our-process-num2.svg'?>" alt="">
-            <p class="our-process_green-text">We will present you with a marketing plan and the services that we can provide to your company in order to make it grow.</p>
+        <div class="center-content">
+            <div class="our-process_process_step">
+                <p class="our-process_process_step_num">2</p>
+                <div class="our-process_process_step_content">
+                    <p class="our-process_green-text">We will present you with a marketing plan and the services that we can provide to your company in order to make it grow.</p>
+                </div>
+            </div>
         </div>
         <div class="our-process_line-separator"></div>
-        <div>
-            <img src="<?= $imgPath.'our-process-num3.svg'?>" alt="">
-            <p class="our-process_green-text">You will choose which of the suggested services you want for your business.</p>
+        <div class="center-content">
+            <div class="our-process_process_step">
+                <p class="our-process_process_step_num">3</p>
+                <div class="our-process_process_step_content">
+                    <p class="our-process_green-text">You will choose which of the suggested services you want for your business.</p>
+                </div>
+            </div>
         </div>
         <div class="our-process_line-separator"></div>
-        <div>
-            <img src="<?= $imgPath.'our-process-num4.svg'?>" alt="">
-            <p class="our-process_big-green-text">We get to work!</p>
+        <div class="center-content">
+            <div class="our-process_process_step">
+                <p class="our-process_process_step_num">4</p>
+                <div class="our-process_process_step_content">
+                    <p class="our-process_big-green-text">We get to work!</p>
+                </div>
+            </div>
         </div>
         <a class="button-gradient">Here is a Breakdown</a>
-        <p class="our-process_button-subtext">of the services we provide</p>
+        <p class="button_lower-text">of the services we provide</p>
     </div>
 </section>
